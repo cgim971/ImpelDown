387334f16e77dea103c8ee181d7923f15fcea694
HyunjunDev <101493216+HyunjunDev@users.noreply.github.com>
2023-02-20 23:10:39 +0900

    훅 수정

--- Client/Assets/Hyunjun/Pirate/Hook.cs
+++ Client/Assets/Hyunjun/Pirate/Hook.cs
@@ -17,12 +17,18 @@ public class Hook : MonoBehaviour
     //플레이어가 닿으면 끌려가는 단순한 구조 나중에는 여기에 닿은 플레이어 움직임이랑 스킬 같은거 못 쓰게 막을 예정 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        transform.position = collision.gameObject.transform.position;
-        player.SkillModule.isAttach = true;
+        if (LayerMask.NameToLayer("Entity") == collision.gameObject.layer)
+        {
+            transform.position = collision.gameObject.transform.position;
+            player.SkillModule.isAttach = true;
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        collision.gameObject.transform.position = transform.position;
+        if (LayerMask.NameToLayer("Entity") == collision.gameObject.layer)
+        {
+            collision.gameObject.transform.position = transform.position;
+        }
     }
 }
